86fe7beea7b2c5336069fb2bdd1d55d2d8d0dbd2
S-ciz <mushinganyacizungu@gmail.com>
2026-01-25 10:21:09 +0200

    image sizing

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,9 +2,10 @@ import { Link } from "react-router";
 
 const ProjectCard = ({project}) => {
     return <Link to={`/project/${project.id}`}>
-     <div className="transition-transform duration-100 hover:transform-[scale(1.01)] mb-2 rounded-xl border border-gray-300 bg-gray-800"> 
+     <div className="transition-transform duration-100 hover:transform-[scale(0.95)] mb-2 rounded-xl border border-gray-300 bg-gray-800"> 
 
-          <img className="w-full h-70 object-cover rounded-t-xl" src={project.image} alt={project.title}/>
+          <img className="w-full h-full object-cover rounded-t-xl" src={project.image} alt={project.title}/>
+         
           <section className="px-5 py-3">
             <div>
 
